1b6b4decf3e12da4f41a9a9187fa741c550fa846
Finch <lukaszzieba4@gmail.com>
2016-05-14 22:05:15 +0100

    Some editor content chnges

--- client/app/admin/admin.config.js
+++ client/app/admin/admin.config.js
@@ -7,7 +7,8 @@
             $stateProvider
                 .state('admin', {
                     url: "/admin",
-                    template: '<admin></admin>',
+                    // template: '<admin></admin>',
+                    templateUrl: './app/admin/admin.html',
                     controller: 'AdminController',
                     data: {
                       requiresLogin: true
--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -8,22 +8,53 @@
     AdminController.$inject = ['$scope', '$sce', 'blogService'];
 
     function AdminController($scope, $sce, blogService) {
-        var vm = this;
-        // vm.post = {};
-        // vm.post.content = '';
-        // vm.htmlOutput = '';
 
-        vm.create = function(post) {
+        $scope.create = function(post) {
+            console.log(post.content);
             blogService.createPost(post)
                 .then(function(response) {
 
                 });
         };
 
-        // $scope.$watch('vm.post.content', function(oldVal, newVal) {
-        //   console.log(newVal);
-        //     vm.htmlOutput = $sce.trustAsHtml(newVal);
-        // });
+        CKEDITOR.replace('content');
+        CKEDITOR.stylesSet.add('my_styles', [
+            // Block-level styles.
+            {
+                name: 'Blue Title',
+                element: 'h2',
+                styles: {
+                    color: 'Blue'
+                }
+            }, {
+                name: 'Red Title',
+                element: 'h3',
+                styles: {
+                    color: 'Red'
+                }
+            },
+
+            // Inline styles.
+            {
+                name: 'CSS Style',
+                element: 'span',
+                attributes: {
+                    'class': 'my_style'
+                }
+            }, {
+                name: 'Marker: Yellow',
+                element: 'span',
+                styles: {
+                    'background-color': 'Yellow'
+                }
+            }
+        ]);
+        $('#submitButton').click(function(e) {
+            $scope.$apply(function() {
+                var editorData = CKEDITOR.instances.content.getData();
+                console.log(editorData);
+            });
+        });
 
         activate();
 
